72221e771fd96ede2066f795051dd723dfe954f1
Ikaro Alef <ikaroalef@gmail.com>
2020-04-23 13:12:09 -0300

    configuração react e gojs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import loadGraph from './loadGraph';
+import Teste from './teste';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    /* const graphContainer = document.querySelector('#graphContainer');
+    const toolbarContainer = document.querySelector('#toolbarContainer');
+    const sidebarContainer = document.querySelector('#sidebarContainer');
+    const outlineContainer = document.querySelector('#outlineContainer');
+    const statusContainer = document.querySelector('#statusContainer');
+    loadGraph(
+      graphContainer,
+      outlineContainer,
+      toolbarContainer,
+      sidebarContainer,
+      statusContainer
+    ); */
+  }
+
+  render() {
+    return <Teste />;
+  }
 }
-
-export default App;
